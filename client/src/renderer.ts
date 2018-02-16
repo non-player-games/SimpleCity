93be92af895d8f3d07cebca502dd8847fa9fbdc1
Eric Liao <rcliao01@gmail.com>
2018-02-15 23:20:28 -0800

    feat: add basic grid display

--- client/src/renderer.ts
+++ client/src/renderer.ts
@@ -1,11 +1,5 @@
+// loading pixi
 require("pixi.js");
-
-let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()) {
-    type = "canvas";
-}
-
-PIXI.utils.sayHello(type);
 
 const pixiApp = new PIXI.Application({
     width: 512,
@@ -13,6 +7,22 @@ const pixiApp = new PIXI.Application({
     antialias: true
 });
 
+// insert pixiApp to document
 document.body.appendChild(pixiApp.view);
 
 pixiApp.renderer.backgroundColor = 0x222222;
+
+const ROW = 5;
+const COLUMN = 5;
+
+for (var i = 0; i < ROW; i ++) {
+    for (var j = 0; j < COLUMN; j ++) {
+        const rectangle = new PIXI.Graphics();
+        rectangle.beginFill(0xFF3333);
+        rectangle.drawRect(0, 0, 40, 40);
+        rectangle.x = i * 50;
+        rectangle.y = j * 50;
+        pixiApp.stage.addChild(rectangle);
+    }
+}
+
