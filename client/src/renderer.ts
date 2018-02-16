8a9b5c476508660852a29ea87d5ab84d996da659
Eric Liao <rcliao01@gmail.com>
2018-02-15 23:03:48 -0800

    feat: add pixi lib

--- client/src/renderer.ts
+++ client/src/renderer.ts
@@ -1,3 +1,18 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+require("pixi.js");
+
+let type = "WebGL";
+if (!PIXI.utils.isWebGLSupported()) {
+    type = "canvas";
+}
+
+PIXI.utils.sayHello(type);
+
+const pixiApp = new PIXI.Application({
+    width: 512,
+    height: 512,
+    antialias: true
+});
+
+document.body.appendChild(pixiApp.view);
+
+pixiApp.renderer.backgroundColor = 0x222222;
