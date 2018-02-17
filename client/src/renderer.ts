d2537dd1e767099a1c7f46901834ddf2c202a4ec
Eric Liao <rcliao01@gmail.com>
2018-02-16 23:18:23 -0800

    feat: add basic interactivity to grid tile

--- client/src/renderer.ts
+++ client/src/renderer.ts
@@ -14,14 +14,33 @@ pixiApp.renderer.backgroundColor = 0x222222;
 const ROW = 5;
 const COLUMN = 5;
 
-for (var i = 0; i < ROW; i ++) {
-    for (var j = 0; j < COLUMN; j ++) {
-        const rectangle = new PIXI.Graphics();
-        rectangle.beginFill(0xFF3333);
-        rectangle.drawRect(0, 0, 40, 40);
-        rectangle.x = i * 50;
-        rectangle.y = j * 50;
-        pixiApp.stage.addChild(rectangle);
+for (let i = 0; i < ROW; i ++) {
+    for (let j = 0; j < COLUMN; j ++) {
+        drawRect(i, j, 0xFF3333, null);
     }
 }
 
+function onclick (rect, i, j) {
+    return function() {
+        console.log('rect', rect);
+        drawRect(i, j, 0x3333FF, rect);
+    }
+}
+
+function drawRect(i, j, color, rect) {
+    let rectangle;
+    if (rect) {
+        rectangle = rect;
+        rect.clear();
+    } else {
+        rectangle = new PIXI.Graphics();
+    }
+    rectangle.beginFill(color);
+    rectangle.drawRect(0, 0, 40, 40);
+    rectangle.x = i * 50;
+    rectangle.y = j * 50;
+    rectangle.interactive = true;
+    rectangle.buttonMode = true;
+    rectangle.on('pointerdown', onclick(rectangle, i, j));
+    pixiApp.stage.addChild(rectangle);
+}
