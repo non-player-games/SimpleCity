e8ce477c3a409da8167483a73536d9bb965c54c3
tonygua <tonygua@users.noreply.github.com>
2018-02-18 22:51:10 -0800

    Add link step for linux/mac

--- client/link-systems.sh
+++ client/link-systems.sh
@@ -1,3 +1,32 @@
 #! /bin/bash
+set -e
 
-cp ../systems/target/debug/libsystems.dylib systems
+if [[ $# -lt 1 ]] ; then
+    echo "Usage: "$(basename $0) TARGET
+    exit 1
+fi
+
+target=$1
+mkdir -p systems
+pushd ../systems
+# @Todo 
+# - add a check for the toolchain
+# - allow --release as an option 
+case "${target}" in
+    "linux")
+        echo "Building for 64 bit linux"
+        cargo build --target=x86_64-unknown-linux-musl
+        cp target/debug/libsystems.so ../client/systems/
+        ;;
+    "darwin")
+        # problem compiling on linux
+        echo "Building for 64 bit mac"
+        cargo build --target=x86_64-apple-darwin
+        mkdir -p ../client/systems
+        cp target/debug/libsystems.dylib ../client/systems/
+        ;;
+    *)
+        echo "unknown target ${target}"
+        exit 1
+        ;;
+esac
